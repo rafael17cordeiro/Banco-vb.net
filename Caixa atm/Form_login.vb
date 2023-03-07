9607f17b5069d0d658a40771496e10ab9577eb7b
rafael17cordeiro <rafarace17cordeiro@gmail.com>
2023-03-07 09:28:01 +0000

    ok

--- Caixa atm/Form_login.vb
+++ Caixa atm/Form_login.vb
@@ -74,49 +74,52 @@ Public Class Form_login
 
 
 
-            If TextBox_user.Text = "" Or TextBox_pin.Text = "" Then
-                MsgBox("Preencha todos os espaços")
+            If num_ut >= 0 And num_ut <= LIM Then
+                If TextBox_user.Text = "" Or TextBox_pin.Text = "" Then
+                    MsgBox("Preencha todos os espaços")
 
-            ElseIf checker_ut = True And num_block(num_ut) <> num_ut And checker_pin = True Then
+                ElseIf checker_ut = True And num_block(num_ut) <> num_ut And checker_pin = True Then
 
-                Timer1.Interval = 5
-                Timer1.Start()
-
-            ElseIf num_ut <> 0 And num_block(num_ut) = num_ut <> 0 Then
-                MsgBox("A sua conta esta Banida . Para recuperacao de conta e necessario acesso administrativo")
-            ElseIf checker_ut = True And checker_pin = False Then
-                MsgBox("Pin não correspondente")
-                TextBox_pin.Text = ""
-                clientes(num_ut, 2) += 1
-                BunifuProgressBar1.Value = 0
+                    Timer1.Interval = 5
+                    Timer1.Start()
 
 
-            ElseIf checker_ut = False And checker_pin = True Then
+                ElseIf checker_ut = True And checker_pin = False And clientes(num_ut, 2) < 3 Then
+                    MsgBox("Pin não correspondente")
+                    TextBox_pin.Text = ""
+                    clientes(num_ut, 2) += 1
+                    BunifuProgressBar1.Value = 0
+
+
+                ElseIf checker_ut = False And checker_pin = True Then
+                    MsgBox("Numero de utilizador não correspondete")
+                    TextBox_user.Text = ""
+                    TextBox_pin.Text = ""
+
+
+
+                ElseIf num_ut <> 69 And pin <> 6969 And clientes(num_ut, 2) < 3 Then
+                    MsgBox("Dados não correspondetes")
+                    TextBox_user.Text = ""
+                    TextBox_pin.Text = ""
+
+
+
+                End If
+            Else
                 MsgBox("Numero de utilizador não correspondete")
                 TextBox_user.Text = ""
                 TextBox_pin.Text = ""
-
-
-
-            ElseIf num_ut <> 69 And pin <> 6969 Then
-                MsgBox("Dados não correspondetes")
-                TextBox_user.Text = ""
-                TextBox_pin.Text = ""
-
-
-
             End If
 
 
-
-
-            If clientes(num_ut, 2) = 3 Then
-                MsgBox("A sua conta foi Suspendida . Para recuperação da conta é necessario acesso administrativo")
-                num_block(num_ut) = num_ut
-                error_conter = 0
+            If num_ut >= 0 And num_ut <= LIM Then
+                If clientes(num_ut, 2) = 3 Then
+                    MsgBox("A sua conta foi bloqueada . Para recuperação da conta é necessario acesso administrativo")
+                    num_block(num_ut) = num_ut
+                    error_conter = 0
+                End If
             End If
-            'nesta secção serve simplesmente para o utilizador saber melhor que erro ele esta a cometer e avisa lo do mesmo
-            '--------------------------- fim secção error checker --------------------------
         ElseIf e.KeyCode = 27 Then
 
             Form_avisoSair.Show()
@@ -219,48 +222,52 @@ Public Class Form_login
 
 
 
-        If TextBox_user.Text = "" Or TextBox_pin.Text = "" Then
-            MsgBox("Preencha todos os espaços")
+        If num_ut >= 0 And num_ut <= LIM Then
+            If TextBox_user.Text = "" Or TextBox_pin.Text = "" Then
+                MsgBox("Preencha todos os espaços")
 
-        ElseIf checker_ut = True And num_block(num_ut) <> num_ut And checker_pin = True Then
+            ElseIf checker_ut = True And num_block(num_ut) <> num_ut And checker_pin = True Then
 
-            Timer1.Interval = 5
-            Timer1.Start()
-
-
-        ElseIf checker_ut = True And checker_pin = False Then
-            MsgBox("Pin não correspondente")
-            TextBox_pin.Text = ""
-            clientes(num_ut, 2) += 1
-            BunifuProgressBar1.Value = 0
+                Timer1.Interval = 5
+                Timer1.Start()
 
 
-        ElseIf checker_ut = False And checker_pin = True Then
+            ElseIf checker_ut = True And checker_pin = False And clientes(num_ut, 2) < 3 Then
+                MsgBox("Pin não correspondente")
+                TextBox_pin.Text = ""
+                clientes(num_ut, 2) += 1
+                BunifuProgressBar1.Value = 0
+
+
+            ElseIf checker_ut = False And checker_pin = True Then
+                MsgBox("Numero de utilizador não correspondete")
+                TextBox_user.Text = ""
+                TextBox_pin.Text = ""
+
+
+
+            ElseIf num_ut <> 69 And pin <> 6969 And clientes(num_ut, 2) < 3 Then
+                MsgBox("Dados não correspondetes")
+                TextBox_user.Text = ""
+                TextBox_pin.Text = ""
+
+
+
+            End If
+        Else
             MsgBox("Numero de utilizador não correspondete")
             TextBox_user.Text = ""
             TextBox_pin.Text = ""
-
-
-
-        ElseIf num_ut <> 69 And pin <> 6969 Then
-            MsgBox("Dados não correspondetes")
-            TextBox_user.Text = ""
-            TextBox_pin.Text = ""
-
-
-
         End If
 
 
-
-
-        If clientes(num_ut, 2) = 3 Then
-            MsgBox("A sua conta foi Suspendida . Para recuperação da conta é necessario acesso administrativo")
-            num_block(num_ut) = num_ut
-            error_conter = 0
+        If num_ut >= 0 And num_ut <= LIM Then
+            If clientes(num_ut, 2) = 3 Then
+                MsgBox("A sua conta foi bloqueada . Para recuperação da conta é necessario acesso administrativo")
+                num_block(num_ut) = num_ut
+                error_conter = 0
+            End If
         End If
-        'nesta secção serve simplesmente para o utilizador saber melhor que erro ele esta a cometer e avisa lo do mesmo
-        '--------------------------- fim secção error checker --------------------------
     End Sub
 
     Private Sub BunifuThinButton22_Click(sender As Object, e As EventArgs) Handles button_sair.Click
